6d838beecda280a70873fe06fd30e18d8741ad77
Elisha Okon <eliscodavid@gmail.com>
2023-04-15 13:02:56 +0000

    upload assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,9 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
+import React from "react";
 
-export default function Home() {
-  return (
-    <div className="container">
-      <main className="main">
-        <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/main.jsx</code>, then modify the{" "}
-          <code className="code">src/App.jsx</code> file!
-        </p>
-
-        <div className="connect">
-          <ConnectWallet dropdownPosition={{
-            align: 'center',
-            side: 'bottom'
-          }} />
+const App = () => {
+    return (
+        <div>
+            <h1>Thirdweb React App</h1>
         </div>
-
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainID, ThirdwebProvider } from "@thirdweb/thirdweb-provider";
 import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const container = document.getElementById("root");
-const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
-      <App />
+    <ThirdwebProvider desiredChainId={ChainID.Sepolia}>
+        <Router>
+            <App />
+        </Router>   
     </ThirdwebProvider>
-  </React.StrictMode>
 );
