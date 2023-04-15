b5030781afa7220979b6d1547cd8a7a7ad92c07c
Elisha Okon <eliscodavid@gmail.com>
2023-04-15 19:07:13 +0000

    updated frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ChainID, ThirdwebProvider } from "@thirdweb/thirdweb-provider";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import { StateContextProvider } from './context';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <ThirdwebProvider desiredChainId={ChainID.Sepolia}>
-        <Router>
-            <App />
-        </Router>   
-    </ThirdwebProvider>
-);
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider> 
+)
