91afeb763ccf9a3dc3f2c9764f8fcb5bcc8fe66d
Elisha Okon <eliscodavid@gmail.com>
2023-04-15 12:27:42 +0000

    Frontend init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { ConnectWallet } from "@thirdweb-dev/react";
+import "./styles/Home.css";
 
-import { Sidebar, Navbar } from './components';
-import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
-
-const App = () => {
+export default function Home() {
   return (
-    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
-      <div className="sm:flex hidden mr-10 relative">
-        <Sidebar />
-      </div>
+    <div className="container">
+      <main className="main">
+        <h1 className="title">
+          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
+        </h1>
 
-      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-        <Navbar />
+        <p className="description">
+          Get started by configuring your desired network in{" "}
+          <code className="code">src/main.jsx</code>, then modify the{" "}
+          <code className="code">src/App.jsx</code> file!
+        </p>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create-campaign" element={<CreateCampaign />} />
-          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
-        </Routes>
-      </div>
+        <div className="connect">
+          <ConnectWallet dropdownPosition={{
+            align: 'center',
+            side: 'bottom'
+          }} />
+        </div>
+
+        <div className="grid">
+          <a href="https://portal.thirdweb.com/" className="card">
+            <h2>Portal &rarr;</h2>
+            <p>
+              Guides, references and resources that will help you build with
+              thirdweb.
+            </p>
+          </a>
+
+          <a href="https://thirdweb.com/dashboard" className="card">
+            <h2>Dashboard &rarr;</h2>
+            <p>
+              Deploy, configure and manage your smart contracts from the
+              dashboard.
+            </p>
+          </a>
+
+          <a href="https://portal.thirdweb.com/templates" className="card">
+            <h2>Templates &rarr;</h2>
+            <p>
+              Discover and clone template projects showcasing thirdweb features.
+            </p>
+          </a>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
-
-export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import "./styles/globals.css";
 
-import { StateContextProvider } from './context';
-import App from './App';
-import './index.css';
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "ethereum";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Sepolia}> 
-    <Router>
-      <StateContextProvider>
-        <App />
-      </StateContextProvider>
-    </Router>
-  </ThirdwebProvider> 
-)
+  <React.StrictMode>
+    <ThirdwebProvider activeChain={activeChain}>
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
