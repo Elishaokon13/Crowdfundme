6ea179ced9c8313f3d1ddf1314e8689c0d43644e
Elisha Okon <eliscodavid@gmail.com>
2023-04-15 12:00:13 +0000

    done with backend

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
-    defaultNetwork: 'goerli',
+    defaultNetwork:'sepolia',
     networks: {
       hardhat: {},
-      goerli: {
-        url: 'https://rpc.ankr.com/eth_goerli',
+      sepolia: {
+        url: 'https://rpc.ankr.com/eth_sepolia',
         accounts: [`0x${process.env.PRIVATE_KEY}`]
       }
     },
