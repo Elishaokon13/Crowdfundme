a53b034b9be1cded2ebb5b76f90f2829dcb6174d
Elisha Okon <eliscodavid@gmail.com>
2023-04-16 11:47:35 +0000

    update CA

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0x4Ae162dF97Ec92FAC77dFd46E388df5De7Ec5Bc9');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
