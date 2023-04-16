ef55ec187e0670929b7084f6132ab0cc366f82a8
Elisha Okon <eliscodavid@gmail.com>
2023-04-16 11:52:05 +0000

    fixed bugs

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -2,7 +2,6 @@ import React, { useContext, createContext } from 'react';
 
 import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
 import { ethers } from 'ethers';
-import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 
 const StateContext = createContext();
 
