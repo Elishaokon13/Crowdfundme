cc76a0d3f094e868e958ef263c6f4969a1343cfe
Elisha Okon <eliscodavid@gmail.com>
2023-04-16 08:59:18 +0000

    made private

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,25 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    global: "globalThis",
-    "process.env": {},
-  },
+    plugins: [react()],
+    define: {
+        global: "globalThis",
+        "process.env": {},
+    },
+    server: {
+        port: 3000,
+    },
+    define: {
+        'process.env': {},
+    },
+    build: {
+        rollupOptions: {
+            plugins: [
+                nodePolyfills(),
+            ],
+        },
+    },
 });
